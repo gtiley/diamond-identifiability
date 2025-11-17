24359457d3165f34421981e79f32281b14ae2e0f
Nan Liu <129575979+lnan22@users.noreply.github.com>
2025-11-17 18:30:17 -0500

    Add files via upload

---
+++ scripts-for-simulation/root3/d8/template2.ctl
@@ -0,0 +1,18 @@
+seed = __SEED__
+seqfile = __SEQFILE__
+treefile = __TREEFILE__
+Imapfile = spMap.txt
+concatfile = __CONCATFILE__
+modelparafile = modelparams.txt
+
+species&tree = 6 A B C D E G
+                 1 1 1 1 1 1
+((C #0.01,D #0.01)u:0.01 #0.01, (((A #0.01,B #0.01)t:0.01 #0.01)k[&phi=0.750000,tau-parent=yes] :0.015 #0.01,((k[&phi=0.250000,tau-parent=yes]:0.015 #0.01,G #0.01)j:0.015 #0.01,E #0.01)v:0.02 #0.01)s:0.03 #0.01)r:0.04 #0.01;
+         phase = 0 0 0 0 0 0
+loci&length = __NLOCI__ __LENGTH__
+locusrate = 0
+clock = 1
+model = 7
+Qrates = 1 3 1 1 1 1 3
+basefreqs = 1 0.3 0.2 0.3 0.2
+alpha_siterate = 1 0.6 4
---
+++ scripts-for-simulation/root3/d9/runbpp.py
@@ -0,0 +1,39 @@
+import os
+
+# Set variables
+diamond = "d9"
+nloci = [250, 1000, 4000]
+length = [250, 1000, 4000]
+
+# Create the submit file name
+submitfile = f"{diamond}.bpp.sh"
+
+# Open the file for writing
+with open(submitfile, 'w') as out1:
+    # Write SLURM directives
+    out1.write(f"#!/bin/bash\n")
+    out1.write(f"#SBATCH --job-name={diamond}.bpp\n")
+    out1.write(f"#SBATCH --output={diamond}.bpp.log\n")
+    out1.write(f"#SBATCH --mail-user=<user_email>\n")  # Replace with actual email
+    out1.write(f"#SBATCH --mail-type=FAIL\n")
+    out1.write(f"#SBATCH --time=24:00:00\n")
+    out1.write(f"#SBATCH --mem-per-cpu=1000M\n")
+    out1.write(f"#SBATCH --nodes=1\n")
+    out1.write(f"#SBATCH --ntasks=1\n")
+    out1.write(f"#SBATCH --cpus-per-task=1\n")
+    out1.write(f"#SBATCH --partition=short\n")
+    # out1.write(f"cd {diamond}\n")
+
+    # Loop through nloci and sequence length and create commands
+    for i in range(3):
+        for j in range(3):
+            for p in range(1,4):
+                for k in range(100):
+                    ctlFile = f"{k}.ctl"
+                    out1.write(f"../software/bpp/src/bpp --simulate phi{p}_{nloci[i]}_{length[j]}/{k}/{ctlFile}\n")
+
+# Run the bash script (use 'sh' or 'bash' based on your preference)
+os.system(f"bash {submitfile}")
+
+# Exit the script
+exit()
